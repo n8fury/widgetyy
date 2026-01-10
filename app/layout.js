e34460ae7d74767e54dff0ddc705f66a0ace4d86
n8fury <mhjoy547@yahoo.com>
2026-01-10 13:48:12 +0600

    style: fix formatting in layout and globals CSS files

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${jetbrainsMono.variable} antialiased`}
-      >
+      <body className={`${jetbrainsMono.variable} antialiased`}>
         {children}
       </body>
     </html>
